741c1a3060d7a25903df0ca623a6b74c080c0fec
EliasFIlho <eliasfilho202@gmail.com>
2025-02-11 08:27:28 -0300

    Fixed projects names and added polling project

--- led_blink/src/main.c
+++ led_blink/src/main.c
@@ -4,7 +4,6 @@
 #include <zephyr/device.h>  
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_NODELABEL(led_0),gpios);
-static const struct gpio_dt_spec bt = GPIO_DT_SPEC_GET(DT_NODELABEL(button_0),gpios);
 
 
 void main(void) {
@@ -16,30 +15,14 @@ void main(void) {
         if(ret < 0){
             return;
         }else{
-            
-        }
-    }
-    if(!device_is_ready(bt.port)){
-        return;
-    }else{
-        ret = gpio_pin_configure_dt(&bt,GPIO_INPUT);  
-        if(ret < 0){
-            return;
-        }else{
-            
+            // just to dont leave a if withou else ;p            
         }
     }
 
-
-    uint8_t value;
     while (1) {
         printk("Hello, Zephyr!\n");
-        value = gpio_pin_get_dt(&bt);
-        printk("Readed value: %d\n",value);
-        if(value == 1){
-            ret = gpio_pin_toggle_dt(&led);
-        }
-        k_sleep(K_MSEC(1000));
+        ret = gpio_pin_toggle_dt(&led);
+        k_sleep(K_MSEC(500));
 
     }
 }
